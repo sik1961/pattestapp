65cd2db3d3045852ca884166069edd1ad19069e3
Si K <simon.kusners@hermes-europe.co.uk>
2018-11-08 15:46:07 +0000

    Add query to get results by customer

--- src/main/java/com/sik/pattest/repository/TestResultsRepository.java
+++ src/main/java/com/sik/pattest/repository/TestResultsRepository.java
@@ -2,8 +2,14 @@ package com.sik.pattest.repository;
 
 import com.sik.pattest.entity.TestResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TestResultsRepository extends JpaRepository<TestResult, String> {
+    @Query(value = "SELECT * FROM  pattest.testresults WHERE customerid=?0",
+            nativeQuery = true)
+    <T extends TestResult> List<T> getTestResultsByCustomerId(String customerId);
 }
--- src/main/java/com/sik/pattest/service/PatDataService.java
+++ src/main/java/com/sik/pattest/service/PatDataService.java
@@ -1,4 +1,36 @@
 package com.sik.pattest.service;
 
+import com.sik.pattest.entity.Customer;
+import com.sik.pattest.entity.TestResult;
+import com.sik.pattest.repository.CustomerRepository;
+import com.sik.pattest.repository.TestResultsRepository;
+
+import java.util.List;
+
 public class PatDataService {
+
+    private CustomerRepository customerRepo;
+    private TestResultsRepository testResultRepo;
+
+    public List<Customer> getAllCustomers() {
+        return this.customerRepo.findAll();
+    }
+
+    public List<TestResult> getAllTestResults() {
+        return this.testResultRepo.findAll();
+    }
+
+    public Customer getCustomerById(String id) {
+        return this.customerRepo.getOne(id);
+    }
+
+    public TestResult getTestResultById(String id) {
+        return this.testResultRepo.getOne(id);
+    }
+
+    public List<TestResult> getTestResultsByCustomerId(String customerId) {
+        return this.testResultRepo.getTestResultsByCustomerId(customerId);
+    }
+
+
 }
