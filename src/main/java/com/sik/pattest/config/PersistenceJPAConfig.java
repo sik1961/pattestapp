20892bf2c882cdb25cb0d0cea5247c448bb80e9a
Si K <simon.kusners@hermes-europe.co.uk>
2018-11-08 15:08:33 +0000

    Fix compile error

--- src/main/java/com/sik/pattest/config/PersistenceJPAConfig.java
+++ src/main/java/com/sik/pattest/config/PersistenceJPAConfig.java
@@ -41,7 +41,7 @@ public class PersistenceJPAConfig{
 
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
+        //em.setJpaProperties(additionalProperties());
 
         return em;
     }
